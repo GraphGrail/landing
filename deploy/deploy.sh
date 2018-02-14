eea4fceeb0f5b04de17acbe3cf8d8ff88ffe8e49
Stanislaw Bogatkin <sbog@sbog.ru>
2018-02-14 18:40:15 +0300

    Fix the difference between deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,8 +16,8 @@ fi
 
 FRONTEND_HEAD_COMMIT=`git rev-parse --verify --short HEAD`
 FRONTEND_REPO=`git config remote.origin.url`
-BACKEND_NAME="landing"
-BACKEND_REPO="git@github.com:graphgrail-front/${BACKEND_NAME}.git"
+BACKEND_NAME="landing_back"
+BACKEND_REPO="git@github.com:graphgrail-front/landing.git"
 
 echo "Prepare the key..."
 # Encryption key is a key stored in travis itself
@@ -29,12 +29,34 @@ echo "Add decoded key to the ssh agent keystore"
 eval `ssh-agent -s`
 ssh-add $OUT_KEY
 
-echo "Add backend repo as backend"
-git remote add backend ${BACKEND_REPO}
+echo "Pull upstream backend repo"
+pushd ..
+git clone $BACKEND_REPO $BACKEND_NAME
+echo "Return back to the original repo"
+popd
 
+echo "Checkout to $BUILD_BRANCH branch in backend repo"
+pushd ../$BACKEND_NAME
+git checkout $BUILD_BRANCH
+popd
+
+echo "Copy data to the backend repo"
+mkdir tmp
+cp -rf ../$BACKEND_NAME/deploy tmp/
+rsync -avi --delete ./ ../$BACKEND_NAME/
+rm -rf ../${BACKEND_NAME}/deploy
+cp -rf tmp/deploy ../${BACKEND_NAME}/
+
+echo "Add new data to the backend repo git"
+pushd ../$BACKEND_NAME
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git add -A .
 if ! [[ -z $(git status -s) ]] ; then
-  echo "Pushing changes to the $BACKEND_REPO $BUILD_BRANCH branch"
-  git push backend $BUILD_BRANCH
+  echo "Pushing changes to the $BACKEND_REPO branch"
+  git commit -m "Add new build data from $BACKEND_NAME frontend $HEAD_COMMIT commit to $BUILD_BRANCH"
+  git push origin $BUILD_BRANCH
   echo "All done."
 else
   echo "There are no changes in result build, so nothing to push forward. End here."
